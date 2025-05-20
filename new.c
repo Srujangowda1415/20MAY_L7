d909236b08360a57be74dd395ff9cae99d98e04c
srujan gowda <srujan.gowda@s.amity.edu>
2025-05-20 11:21:41 +0530

    Update new.c

--- new.c
+++ new.c
@@ -1,1 +1,20 @@
-this is a c file 
+#include <stdio.h>
+
+int main() {
+    int rows, i, j, space;
+
+    printf("Enter the number of rows for the triangle: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        for (space = 1; space <= rows - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
