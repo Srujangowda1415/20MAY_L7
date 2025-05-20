35d8a2477160175a6599348c19c0f6b41f485b1c
srujan gowda <srujan.gowda@s.amity.edu>
2025-05-20 11:21:13 +0530

    Update new.c

--- new.c
+++ new.c
@@ -1,1 +1,16 @@
-this is a c file 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter the no of rowsa: ");
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++) {
+            for (int k = 10; k >= (i + 2); k--) {
+                printf("%d ", k);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
